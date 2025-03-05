5ca53d63ce20e8e11fbe5b8c817ad3c94062b865
casmir <casmir@hetzwolshalfuurtje.nl>
2025-03-05 19:55:51 +0100

    Update 1.0__initial.sql

--- src/main/resources/dbmigration/1.0__initial.sql
+++ src/main/resources/dbmigration/1.0__initial.sql
@@ -1,6 +1,6 @@
 -- apply changes
 create table product_history (
-  id                            bigint(19) not null,
+  id                            bigint not null,
   other                         varchar(50) not null,
   name                          varchar(50),
   constraint pk_product_history primary key (id,other)
